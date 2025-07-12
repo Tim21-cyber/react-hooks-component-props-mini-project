83c5aa5b48ff4ea0e632b385c46ce08b8e3dc719
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-07-12 14:40:21 +0300

    complete lab

---
+++ src/components/Article.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    const renderEmojis = () => {
+        if (minutes < 30) {
+            const cups = Math.ceil(minutes / 5);
+            return "☕".repeat(cups);
+        }
+        else {
+            const bentos = Math.ceil(minutes / 10);
+            return "🍱".repeat(bentos);
+        }
+    }
+
+    return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {renderEmojis()} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+export default Article;
+
